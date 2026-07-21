5e3d3b86d3f4669fb6bb4c85a1d204f114ca5c93
Alaguraja787 <alaguraja698900@gmail.com>
2026-07-21 11:23:58 +0530

    Time: 1 ms (77.93%), Space: 42.4 MB (51.56%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean isHappy(int n){
         HashSet<Integer> set  = new HashSet<>();
         while (n != 0){
-            set.add(n);
             int temp = n;
             int sum = 0;
             while (temp != 0){
@@ -14,9 +13,10 @@ class Solution {
                 return true;
             }
             n = sum;
-            if (set.contains(n)){
+            if (!set.add(n)){
                 return false;
             }
+            set.add(n);
         }
         return false;
         
