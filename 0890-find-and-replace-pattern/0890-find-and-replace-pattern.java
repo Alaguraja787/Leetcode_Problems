5fefdcb4703f93b9fb02255ccf9b4e741dfa1820
Alaguraja787 <alaguraja698900@gmail.com>
2026-07-23 15:52:02 +0530

    Time: 1 ms (80.19%), Space: 44.2 MB (23.47%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
@@ -2,31 +2,28 @@ class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         ArrayList<String> res = new ArrayList<>();
         for (String s: words){
-            if(ismatch(s,pattern)){
+            if (ismatch(s, pattern)){
                 res.add(s);
             }
         }
-
-
-    
         return res;
     }
-    private boolean ismatch(String word,String pat){
-            HashMap<Character, Character> map1 = new HashMap<>();
-            HashMap<Character, Character> map2 = new HashMap<>();
-            for (int i=0; i<word.length(); i++){
-                char p = pat.charAt(i);
-                char w = word.charAt(i);
-                if (!map1.containsKey(p) && !map2.containsKey(w)){
-                    map1.put(p, w);
-                    map2.put(w,p);
-                }
-                else{
-                    if (map1.getOrDefault(p, '#') != w || (map2.getOrDefault(w,'#') != p)){
-                        return false;
-                    }
+    private boolean ismatch(String s, String pattern){
+        HashMap<Character, Character> map1 = new HashMap<>();
+        HashMap<Character, Character> map2 = new HashMap<>();
+        for (int i=0; i<s.length(); i++){
+            char p = pattern.charAt(i);
+            char w = s.charAt(i);
+            if (!map1.containsKey(p) && !map2.containsKey(w)){
+                map1.put(p,w);
+                map2.put(w,p);
+            }
+            else{
+                if (map1.getOrDefault(p,'#') != w || map2.getOrDefault(w,'#') !=p){
+                    return false;
                 }
             }
-            return true;
         }
+        return true;
+    }
 }
