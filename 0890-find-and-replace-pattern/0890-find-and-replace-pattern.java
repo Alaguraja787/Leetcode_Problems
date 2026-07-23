6125c65ba347201d45944ffee6789bc13131a1bb
Alaguraja787 <alaguraja698900@gmail.com>
2026-07-23 21:32:24 +0530

    Time: 1 ms (80.19%), Space: 44.2 MB (23.47%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
@@ -1,25 +1,27 @@
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         ArrayList<String> res = new ArrayList<>();
-        for (String s: words){
-            if (ismatch(s, pattern)){
+        for(String s: words){
+            if (ismatch(s,pattern)){
                 res.add(s);
             }
         }
         return res;
     }
+
     private boolean ismatch(String s, String pattern){
         HashMap<Character, Character> map1 = new HashMap<>();
         HashMap<Character, Character> map2 = new HashMap<>();
         for (int i=0; i<s.length(); i++){
-            char p = pattern.charAt(i);
-            char w = s.charAt(i);
-            if (!map1.containsKey(p) && !map2.containsKey(w)){
-                map1.put(p,w);
-                map2.put(w,p);
+            char pat = pattern.charAt(i);
+            char ss = s.charAt(i);
+
+            if (!map1.containsKey(pat) && !map2.containsKey(ss)){
+                map1.put(pat,ss);
+                map2.put(ss,pat);
             }
             else{
-                if (map1.getOrDefault(p,'#') != w){
+                if(map1.getOrDefault(pat,'#') !=ss || (map2.getOrDefault(ss,'#') != pat)){
                     return false;
                 }
             }
