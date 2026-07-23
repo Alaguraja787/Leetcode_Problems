30bf1412329994ed059a02e5d2bdc4066389ff1d
Alaguraja787 <alaguraja698900@gmail.com>
2026-07-23 09:46:57 +0530

    Time: 9 ms (21.94%), Space: 43.5 MB (38.06%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -3,7 +3,9 @@ class Solution {
         HashMap<Character, Integer> map = new HashMap<>();
         for (char i :s.toCharArray()){
             map.put(i, map.getOrDefault(i,0)+1);
+            
         }
+        
         char ch = s.charAt(0);
         int freq = map.get(ch);
         for (char j:s.toCharArray()){
