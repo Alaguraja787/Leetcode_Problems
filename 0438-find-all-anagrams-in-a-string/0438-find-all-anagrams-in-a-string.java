574cc7d4af2a77bac063ec947b35c13c0b88ec42
Alaguraja787 <alaguraja698900@gmail.com>
2026-07-25 11:46:14 +0530

    Time: 65 ms (19.39%), Space: 48.2 MB (5.43%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,34 +1,36 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         ArrayList<Integer> res = new ArrayList<>();
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
-        if (p.length() > s.length()) {
+        if (p.length()>s.length()){
             return res;
         }
-        for (char i: p.toCharArray()){
-            map1.put(i, map1.getOrDefault(i,0)+1);
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        for (char x:p.toCharArray()){
+            map1.put(x, map1.getOrDefault(x,0)+1);
         }
 
-        for (int j=0; j<p.length(); j++){
-            map2.put(s.charAt(j),map2.getOrDefault(s.charAt(j),0)+1);
+        for (int i=0;i<p.length(); i++){
+            char ch=s.charAt(i);
+            map2.put(ch, map2.getOrDefault(ch,0)+1);
         }
+
         int left =0;
         if (map1.equals(map2)){
             res.add(left);
         }
+
         for (int right=p.length();right<s.length();right++){
-            map2.put(s.charAt(left),map2.getOrDefault(s.charAt(left),0)-1);
+            map2.put(s.charAt(left), map2.getOrDefault(s.charAt(left),0)-1);
             if (map2.get(s.charAt(left))==0){
                 map2.remove(s.charAt(left));
             }
-            map2.put(s.charAt(right),map2.getOrDefault(s.charAt(right),0)+1);
             left++;
+            map2.put(s.charAt(right), map2.getOrDefault(s.charAt(right),0)+1);
 
             if (map1.equals(map2)){
                 res.add(left);
             }
-            
         }
         return res;
     }
