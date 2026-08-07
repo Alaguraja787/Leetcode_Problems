95160b634222e5224bfd6a1faae77a513fd29c82
Alaguraja787 <alaguraja698900@gmail.com>
2026-08-07 10:09:46 +0530

    Time: 15 ms (31.49%), Space: 47 MB (34.47%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,25 +1,21 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         HashMap<Character, Integer> map = new HashMap<>();
-        for (int i=0; i<magazine.length(); i++){
-            map.put(magazine.charAt(i), map.getOrDefault(magazine.charAt(i), 0)+1);
+        for (char ch: magazine.toCharArray()){
+            map.put(ch, map.getOrDefault(ch,0)+1);
         }
-
-        for (int j=0; j<ransomNote.length(); j++){
-            if (map.containsKey(ransomNote.charAt(j))){
-                map.put(ransomNote.charAt(j), map.getOrDefault(ransomNote.charAt(j), 0)-1);
-                if (map.get(ransomNote.charAt(j))<0){
-                    return false;
+        for (char c: ransomNote.toCharArray()){
+            if (map.containsKey(c)){
+                map.put(c, map.getOrDefault(c,0)-1);
+                if (map.get(c)==0){
+                    map.remove(c);
                 }
             }
-            if (!map.containsKey(ransomNote.charAt(j))){
+            else{
                 return false;
-    
             }
         }
-        return true;
 
-
-
+        return true;        
     }
 }
