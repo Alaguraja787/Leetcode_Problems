357ef81b36fe1a6004f90912feaca11f984ba300
Alaguraja787 <alaguraja698900@gmail.com>
2026-09-01 15:22:00 +0530

    Time: 29 ms (6.35%), Space: 50.1 MB (17.91%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -4,7 +4,6 @@ class Solution {
         for (int i=1; i<=n; i++){
             map.put(i, new ArrayList<>());
         }
-
         for (int[] edge: times){
             int u = edge[0];
             int v = edge[1];
@@ -13,35 +12,35 @@ class Solution {
             map.get(u).add(new ArrayList<>(Arrays.asList(v,w)));
         }
 
-         
         int[] distance = new int[n+1];
         Arrays.fill(distance, Integer.MAX_VALUE);
         distance[k] = 0;
-       PriorityQueue<List<Integer>> q =
-            new PriorityQueue<>(
-                (a, b) -> Integer.compare(a.get(1), b.get(1))
-            );
-        q.offer(new ArrayList<>(Arrays.asList(k,0)));
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<>(
+            (a,b) -> Integer.compare(a.get(1), b.get(1))
+        );
+        pq.offer(new ArrayList<>(Arrays.asList(k,0)));
 
-        while (!q.isEmpty()){
-            List<Integer> curr = q.poll();
+        while (!pq.isEmpty()){
+            List<Integer> curr = pq.poll();
             int node = curr.get(0);
             int weight = curr.get(1);
-            if (weight>distance[node]){
+
+            if (distance[node]<weight){
                 continue;
             }
+
             for (List<Integer> nei: map.get(node)){
                 int newweight = weight+nei.get(1);
-                if (newweight<distance[nei.get(0)]){
+                if (newweight < distance[nei.get(0)]){
                     distance[nei.get(0)] = newweight;
-                    q.offer(new ArrayList<>(Arrays.asList(nei.get(0), distance[nei.get(0)])));
+                    pq.offer(new ArrayList<>(Arrays.asList(nei.get(0), distance[nei.get(0)])));
                 }
             }
-        } 
+        }
         int ans = 0;
         for (int i=1; i<=n; i++){
             if (distance[i] == Integer.MAX_VALUE){
-                    return -1;
+                return -1;
             }
             ans = Math.max(ans, distance[i]);
         }
